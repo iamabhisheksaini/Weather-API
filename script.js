11fdfef5a2198cf164b5ad83ead2b96c943d602a
Abhishek Saini <124495434+iamabhisheksaini@users.noreply.github.com>
2024-02-28 17:58:07 +0530

    changes

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
 const searchInput = document.querySelector('.city-input');
 const searchBtn = document.querySelector('.search-btn');
+const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city) {
   try {
@@ -15,6 +16,33 @@ async function checkWeather(city) {
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+    
+     if (data.weather[0].main == "Clouds"){
+      weatherIcon.src = "images/clouds.png";
+
+     }
+     else if(data.weather[0].main == "clear"){
+      weatherIcon.src = " images/clear.png";
+     }
+     
+
+     else if(data.weather[0].main == "Rain"){
+      weatherIcon.src = " images/rain.png";
+     }
+    
+     else if(data.weather[0].main == "Drizzle"){
+      weatherIcon.src = " images/drizzle.png";
+     }
+     
+     else if(data.weather[0].main == "Mist"){
+      weatherIcon.src = " images/mist.png";
+     }
+
+      
+
+     document.querySelector(".weather").style.display = "block";
+  
+  
   } catch (error) {
     console.error("Error fetching weather data:", error);
   }
